09e1bcf6bf30aad038d96c5683aa26a479ee0f82
havelyn325 <havelyn325@gmail.com>
2021-06-07 23:26:32 +0700

    started this project

---
+++ content/scripts/mousePositionToCss.js
@@ -0,0 +1,11 @@
+const root = document.documentElement;
+var offsetX = 16;
+var offsetY = 8;
+
+document.addEventListener('mousemove', evt => {
+	
+	let x = (evt.clientX + offsetX) / innerWidth;
+	let y = (evt.clientY + offsetY) / innerHeight;
+    root.style.setProperty('--mouse-x', x);
+    root.style.setProperty('--mouse-y', y);
+});
---
+++ content/scripts/pageGenerator.js
@@ -0,0 +1,15 @@
+
+
+var imgPath = "content/img/";
+var template = "
+
+";
+
+function generateList(class,) 
+{
+	let strr = '<div class="equipEntry"><img src="content/img/1.png"></div>';
+	for (let i = 0; i < 48; i++) {
+		document.getElementById("equipmentList").innerHTML += strr;
+	}
+	
+}	
