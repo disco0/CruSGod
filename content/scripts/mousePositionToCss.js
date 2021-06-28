83aeeafc52aa9b99279769afc4c779cdab224aa9
havelyn325 <30593254+havelyn325@users.noreply.github.com>
2021-06-28 16:41:32 +0700

    Update mousePositionToCss.js

--- content/scripts/mousePositionToCss.js
+++ content/scripts/mousePositionToCss.js
@@ -1,11 +1,13 @@
-const root = document.documentElement;
-var offsetX = 16;
-var offsetY = 8;
+{
+	const root = document.documentElement;
+	var offsetX = 16;
+	var offsetY = 8;
 
-document.addEventListener('mousemove', evt => {
-	
-	let x = (evt.clientX + offsetX) / innerWidth;
-	let y = (evt.clientY + offsetY) / innerHeight;
-    root.style.setProperty('--mouse-x', x);
-    root.style.setProperty('--mouse-y', y);
-});
+	document.addEventListener('mousemove', evt => {
+
+		let x = (evt.clientX + offsetX) / innerWidth;
+		let y = (evt.clientY + offsetY) / innerHeight;
+	    root.style.setProperty('--mouse-x', x);
+	    root.style.setProperty('--mouse-y', y);
+	});
+}
