a75530586ea3185edc17b88ff80cf282a666b353
havelyn325 <30593254+havelyn325@users.noreply.github.com>
2021-06-28 16:41:17 +0700

    Update rightPanelResizer2.js

--- content/scripts/rightPanelResizer2.js
+++ content/scripts/rightPanelResizer2.js
@@ -1,35 +1,37 @@
-const root = document.documentElement;
-var rightContainer;
+{
+	const root = document.documentElement;
+	var rightContainer;
 
-function onStart() {
-	rightContainer = document.getElementById("rightContainer");
-}
-
-function resize() {
-	var leftOffset = rightContainer.offsetLeft;
-	var topOffset = rightContainer.offsetTop;	
-	
-	var style = window.getComputedStyle(rightContainer, null);
-	var paddingH = parseInt(style.getPropertyValue("padding-left"), 10) + parseInt(style.getPropertyValue("padding-right"), 10);
-	var paddingV = parseInt(style.getPropertyValue("padding-top"), 10) + parseInt(style.getPropertyValue("padding-bottom"), 10);
-	var contentWidth = innerWidth - leftOffset - paddingH; //avaliable
-	var contentHeight = innerHeight - topOffset - paddingV;
-	
-	
-	var contentSize = Math.min(contentWidth, contentHeight);
-	//var padding = parseInt(style.getPropertyValue("padding"), 10)
-	if (topOffset > 0) 
-	{
-		rightContainer.style.setProperty("justify-content", "center");
-		} else {
-		rightContainer.style.setProperty("justify-content", "start");
+	function onStart() {
+		rightContainer = document.getElementById("rightContainer");
 	}
-	
-	root.style.setProperty("--rightContainerSize", contentSize + "px");
-	console.info(paddingH, paddingV);
-	
-}
 
-window.addEventListener('load', onStart);
-window.addEventListener('load', resize);
-window.addEventListener('resize', resize);
+	function resize() {
+		var leftOffset = rightContainer.offsetLeft;
+		var topOffset = rightContainer.offsetTop;	
+
+		var style = window.getComputedStyle(rightContainer, null);
+		var paddingH = parseInt(style.getPropertyValue("padding-left"), 10) + parseInt(style.getPropertyValue("padding-right"), 10);
+		var paddingV = parseInt(style.getPropertyValue("padding-top"), 10) + parseInt(style.getPropertyValue("padding-bottom"), 10);
+		var contentWidth = innerWidth - leftOffset - paddingH; //avaliable
+		var contentHeight = innerHeight - topOffset - paddingV;
+
+
+		var contentSize = Math.min(contentWidth, contentHeight);
+		//var padding = parseInt(style.getPropertyValue("padding"), 10)
+		if (topOffset > 0) 
+		{
+			rightContainer.style.setProperty("justify-content", "center");
+			} else {
+			rightContainer.style.setProperty("justify-content", "start");
+		}
+
+		root.style.setProperty("--rightContainerSize", contentSize + "px");
+		console.info(paddingH, paddingV);
+
+	}
+
+	window.addEventListener('load', onStart);
+	window.addEventListener('load', resize);
+	window.addEventListener('resize', resize);
+}
