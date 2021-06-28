cd259de47c05e79297a5021e86b17eb004d7b846
havelyn325 <30593254+havelyn325@users.noreply.github.com>
2021-06-28 13:51:45 +0700

    Update rightPanelResizer.js

--- content/scripts/rightPanelResizer.js
+++ content/scripts/rightPanelResizer.js
@@ -1,7 +1,9 @@
 var id = "rightContainer";
 function setRightPanelHeight() {
 	rightPanel = document.getElementById(id);	
+	console.info(innerHeigth, rightPanel.offsetTop);
 	rightPanel.style.height = innerHeight - rightPanel.offsetTop + "px";
+	
 }
 window.addEventListener('resize', setRightPanelHeight);
 window.addEventListener('DOMContentLoaded', setRightPanelHeight);
