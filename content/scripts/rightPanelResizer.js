400ca9aef18c0dc240491b53419fb1311abb018c
havelyn325 <30593254+havelyn325@users.noreply.github.com>
2021-06-28 14:18:32 +0700

    Update rightPanelResizer.js

--- content/scripts/rightPanelResizer.js
+++ content/scripts/rightPanelResizer.js
@@ -1,8 +1,10 @@
 var id = "rightContainer";
 function setRightPanelHeight() {
-	rightPanel = document.getElementById(id);	
-	console.info(innerHeight, rightPanel.offsetTop);
-	rightPanel.style.height = innerHeight - rightPanel.offsetTop + "px";
+	rightPanel = document.getElementById(id);
+	var style = window.getComputedStyle(rightPanel, null);
+	var padding = parseInt(style.getPropertyValue("padding-bottom"), 10);
+	padding += parseInt(style.getPropertyValue("padding-top"), 10);
+	rightPanel.style.height = innerHeight - rightPanel.offsetTop - padding + "px";
 	
 }
 window.addEventListener('resize', setRightPanelHeight);
