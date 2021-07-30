b9c0ea0cbac3c16b7e2fbaad63a1949fc9b0ecc7
havelyn325 <30593254+havelyn325@users.noreply.github.com>
2021-07-30 17:39:08 +0700

    Update pageGenerator.js

--- content/scripts/pageGenerator.js
+++ content/scripts/pageGenerator.js
@@ -1,20 +1,21 @@
-function generateList(containerId, columnsCount, rowsCount) 
+function generateList(containerId, totalCount, columnsCount) 
 {
-	var strr = '<div class="leftSubGridEntry"><img src="1.png"></div>';
+	var strr = '<div class="leftSubGridEntry"><img src="1.png"></div>';	
 	var container = document.getElementById(containerId);
 	var animationSpeedDivider = 20;
+	var rowsCount = Math.ceil(totalCount / columnsCount);
 	container.style.setProperty("grid-template-columns", "repeat(" + columnsCount + ", 64px)");
-	for (var i = 0; i < columnsCount * rowsCount; i++) {
+	for (let i = 0; i < totalCount; i++) {
 		container.innerHTML += strr;
 		let cell = container.lastElementChild;
 		var delay;
 		if (i < columnsCount) {
 			delay = i;							
-			} else {	
+		} else {	
 			delay = columnsCount + Math.trunc(i / columnsCount);
-			}
+		}
 		delay /= animationSpeedDivider + (0.5 - Math.random()) * 1;
 		//delay /= animationSpeedDivider;
 		cell.style.setProperty("--delay", delay + "s");
 		container.style.setProperty("--max-delay", delay + "s");
-}	
+}}
