57b039808d7caecb7bda52ac1c543809635cc1f1
havelyn325 <30593254+havelyn325@users.noreply.github.com>
2021-08-05 10:47:14 +0700

    Update pageGenerator.js

--- content/scripts/pageGenerator.js
+++ content/scripts/pageGenerator.js
@@ -1,10 +1,10 @@
 function createGrid(containerId, totalCount, columnsCount){
-	var strr = '<div class="leftSubGridEntry"><img src="1.png"></div>';	
+	var strr = '<div class="leftSubGridEntry"><img src="content/img/1.png"></div>';	
 	var container = document.getElementById(containerId);
 	var animationSpeedDivider = 20;
 	var rowsCount = Math.ceil(totalCount / columnsCount);
 	container.style.setProperty("grid-template-columns", "repeat(" + columnsCount + ", 64px)");
-	console.log(containerId);
+	console.log(containerId);	
 	for (var i = 0; i < totalCount; i++) {
 		container.innerHTML += strr;
 		let cell = container.lastElementChild;
@@ -13,12 +13,13 @@ function createGrid(containerId, totalCount, columnsCount){
 			delay = i;							
 			} else {	
 			delay = columnsCount + Math.trunc(i / columnsCount);
+			console.log(delay);
 		}
 		delay /= animationSpeedDivider + (0.5 - Math.random()) * 1;
 		//delay /= animationSpeedDivider;
 		cell.style.setProperty("--delay", delay + "s");
 		container.style.setProperty("--max-delay", delay + "s");
-		console.log(delay);
+		//console.log(delay);
 	}	
 	console.log("===========");
-	}					
+}					
