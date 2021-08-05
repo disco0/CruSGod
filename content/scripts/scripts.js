a40b9ff6ec494ef523e5f7de170fcd4ba3305032
havelyn325 <30593254+havelyn325@users.noreply.github.com>
2021-08-05 17:09:38 +0700

    Update scripts.js

--- content/scripts/scripts.js
+++ content/scripts/scripts.js
@@ -1,25 +1,25 @@
+//Writes mouse position to CSS variables var(--mouse-x) and var(--mouse-y)
 {
-  const root = document.documentElement;
+	const root = document.documentElement;
 	var offsetX = 16;
 	var offsetY = 8;
-
 	document.addEventListener('mousemove', evt => {
-
 		let x = (evt.clientX + offsetX) / innerWidth;
 		let y = (evt.clientY + offsetY) / innerHeight;
-	    root.style.setProperty('--mouse-x', x);
-	    root.style.setProperty('--mouse-y', y);
+		root.style.setProperty('--mouse-x', x);
+		root.style.setProperty('--mouse-y', y);
 	});
 }
 
-function createGrid(containerId, totalCount, columnsCount){
+//Creates grid in containerId with totalCount elements and columnsCount rows
+function createGrid(containerId, totalCount, columnsCount) {
 	totalCount=parseInt(totalCount);
 	columnsCount=parseInt(columnsCount);	
   
-  var cellClass = "leftSubGridEntry";
-  var cellImgSrc = "content/img/1.png";
-  var animationSpeedDivider = 20;
-  var randomStrength = 1;
+	var cellClass = "leftSubGridEntry";
+	var cellImgSrc = "content/img/1.png";
+	var animationSpeedDivider = 20;
+	var randomStrength = 1;
   
 	var strr = `<div class="${cellClass}"><img src="${cellImgSrc}"></div>`;	
 	var container = document.getElementById(containerId);	
@@ -44,12 +44,12 @@ function createGrid(containerId, totalCount, columnsCount){
 
 //Grid images randomizer
 function randomizeGrid(){
-  Array.from(document.querySelectorAll('.leftSubGridEntry > img')).forEach(el => 
-  {
-    var positions = ['0%', '33%', '66%', '100%'];
-    var position = positions[Math.floor(Math.random() * positions.length)]
-    el.style.objectPosition = position + ' top';
-    console.info(el.objectPosition);		
-  });
+	Array.from(document.querySelectorAll('.leftSubGridEntry > img')).forEach(el => 
+	{
+		var positions = ['0%', '33%', '66%', '100%'];
+		var position = positions[Math.floor(Math.random() * positions.length)]
+		el.style.objectPosition = position + ' top';
+		console.info(el.objectPosition);		
+	});
 }
 
